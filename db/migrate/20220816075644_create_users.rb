7ae77d6bc2be5914963faaee3407603229324eb9
juwpan <juwpan@gmail.com>
2022-08-22 13:41:58 +0400

    stylesheet for devise

--- db/migrate/20220816075644_create_users.rb
+++ db/migrate/20220816075644_create_users.rb
@@ -2,11 +2,10 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :surname
-      t.string :email
-      t.string :nickname
-      t.date :age
+      t.string :nickname, unique: true
+      t.date :birth_date
       t.string :avatar
+      t.string :email
 
       t.timestamps
     end
