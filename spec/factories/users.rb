216f4f27bf676a377456e14b615a7197bee4c920
juwpan <juwpan@gmail.com>
2022-08-26 22:26:42 +0400

    test rspec user model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :user do
     name { "мишка_#{rand(999)}" }
     birth_date { "15.07.2001" }
-    gender {"#{["м", "ж"].sample}"}
+    gender { "м" }
+    confirmed_at { Time.now }
 
-    sequence(:email) { |n| "juwpan_#{n}@gmail.com" }
+    sequence(:id) { |n| "#{n + 1}"}
+    sequence(:email) { |n| "juwpan_15@gmail.com" }
     sequence(:nickname) { |n| "rumor_#{n}" }
-
-    # is_admin { false }
 
     after(:build) { |u| u.password_confirmation = u.password = "1"}
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,11 +22,8 @@ RSpec.describe User, type: :model do
     # Проверяем уникальность
     context 'when nickname uniquess' do
       subject { FactoryBot.build(:user) }
-
       # Уникальный ник
       it { should validate_uniqueness_of(:nickname) }
-      # #
-      # it { should validate_uniqueness_of(:email) }
     end
   end
 
@@ -34,9 +31,51 @@ RSpec.describe User, type: :model do
   describe '#capitalize' do
     context 'when capitalized' do
       let(:user) { create(:user) }
+
       # Должен вернуть заглавную букву имени
       it 'should return name.capitalize' do
         expect(user.name[0].first).to eq('М')
+      end
+    end
+  end
+
+  describe 'attributes user' do
+    context 'when correct valid attributes' do
+      let(:user) { create(:user) }
+
+      # Должен вернуть правильное имя
+      it 'should return correct name' do
+        expect(user.name).to include('Мишка')
+      end
+
+      # Должен вернуть правильную дату
+      it 'should return correct birth_date' do
+        expect(user.birth_date).to eq(Date.parse("15.07.2001"))
+      end
+
+      # Должен вернуть правильный пол
+      it 'should return correct gender' do
+        expect(user.gender).to eq('м')
+      end
+
+      # Должен вернуть правильный ник
+      it 'should return correct nickname' do
+        expect(user.nickname).to include('rumor')
+      end
+
+      # Должен вернуть правильную почту
+      it 'should return correct email' do
+        expect(user.email).to eq('juwpan_15@gmail.com')
+      end
+
+      # Должен вернуть правильный ник
+      it 'should return correct password_confirmation' do
+        expect(user.password_confirmation == user.password = '1').to eq(true)
+      end
+      
+      # Должен вернуть правильный ник
+      it 'should return correct confirmed_at' do
+        expect(user.confirmed_at).to be_truthy
       end
     end
   end
